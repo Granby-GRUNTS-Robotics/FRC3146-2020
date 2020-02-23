fe7f9b0ed056c6608be1cd8b03b1e88bb9324d07
Team Laptop <team3146@gmail.com>
2020-02-23 15:18:21 -0500

    Redid Class
    Added solenoids and functions

--- src/main/java/frc/robot/subsystems/ShooterLift.java
+++ src/main/java/frc/robot/subsystems/ShooterLift.java
@@ -10,22 +10,48 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.PneumaticConstants;
+import static frc.robot.Constants.PneumaticConstants.kON;
+import static frc.robot.Constants.PneumaticConstants.kOFF;
+
 
 public class ShooterLift extends SubsystemBase {
   /**
    * Creates a new ShooterLift.
    */
-  private final Solenoid leftBottomSolenoid = new Solenoid(PneumaticConstants.kLOW_LEFT_LIFT_PORT);
-  private final Solenoid rightBottomSolenoid = new Solenoid(PneumaticConstants.kLOW_RIGHT_LIFT_PORT);
+  private final Solenoid topSolenoid = new Solenoid(PneumaticConstants.kPCM_Port, PneumaticConstants.kTOP_SHOOTER_LIFT_PORT);
+  private final Solenoid bottomSolenoid = new Solenoid(PneumaticConstants.kPCM_Port, PneumaticConstants.kBOTTOM_SHOOTER_LIFT_PORT);
+
 
 
   public ShooterLift() {
 
   }
 
-  public final void up(){
-    leftBottomSolenoid.set(PneumaticConstants.kSHOOTER_UP);
-    rightBottomSolenoid.set(PneumaticConstants.kSHOOTER_UP);
+  public final void setPosition(int pos){
+    /*Case defines which 
+     *position the shooter lift will go to. the pistons are different
+     *sizes so we can use different combinations for different positions
+     */
+    switch (pos) {
+      case 0:
+        topSolenoid.set(kOFF);
+        bottomSolenoid.set(kOFF);
+        break;
+      case 1:
+        topSolenoid.set(kOFF);
+        bottomSolenoid.set(kON);
+        break;
+      case 2:
+        topSolenoid.set(kON);
+        bottomSolenoid.set(kOFF);
+      case 3:
+        topSolenoid.set(kON);
+        bottomSolenoid.set(kON);
+      default:
+      topSolenoid.set(kON);
+      bottomSolenoid.set(kOFF);
+        break;
+    }
   }
 
   @Override
